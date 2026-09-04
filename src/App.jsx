6e3c23caa839becfdd3eb134311c4ed1f9993e72
shashikaherath <shashikapalamakumbura@gmail.com>
2026-09-04 11:22:23 +0530

    add a Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-//import Home from './pages/Home';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Home from './pages/Home';
 import SearchPage from './pages/SearchPage';
-//import About from './pages/About';
+
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 export default function App() {
-  return (
-      <Router>
-          <div className="app-wrapper">
-          <Header />
-          <main className="main-content">
-            <Routes>
-                <Route path="/search" element={<SearchPage />} />
-              {/*
-              <Route path="/" element={<Home />} />
-              <Route path="/search" element={<SearchPage />} />
-              <Route path="/about" element={<About />} />
-              */}
-            </Routes>
-          </main>
-          <Footer />
-        </div>
-      </Router>
-  );
+    return (
+        <Router>
+            <div className="app-wrapper">
+                <Header />
+
+                <main className="main-content">
+                    <Routes>
+                        {/* Default/Home page */}
+                        <Route path="/" element={<Home />} />
+
+                        {/* Search page */}
+                        <Route path="/search" element={<SearchPage />} />
+                    </Routes>
+                </main>
+
+                <Footer />
+            </div>
+        </Router>
+    );
 }
