ea8a197ad9ff847e34ca4ccab8d1ba67f3a071c7
Mudalige T.M.G.T.D <it24102197@my.sliit.lk>
2026-09-04 11:25:35 +0530

    header, footer navigation

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,13 +7,17 @@ export default function Header({ appName = "Medicine Finder", countryTag = "SL" 
                 {/* Brand Logo & Title */}
                 <div className="brand-logo">
                     <span style={{ fontSize: '1.5rem' }}>💊</span>
-                    <span className="brand-title">
-            {appName} <span className="badge-country">{countryTag}</span>
-          </span>
+                    <a href="/about" className="nav-link">
+                        <span className="brand-title">
+                            Medicine Finder<span className="badge-country">SL</span>
+                        </span>
+                    </a>
+
                 </div>
 
                 {/* Navigation Links */}
                 <nav className="app-nav">
+                    <a href="/search" className="nav-link">Search</a>
                     <a href="/about" className="nav-link">About</a>
                 </nav>
             </div>
